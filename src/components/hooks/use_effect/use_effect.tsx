488ef1559f99592833098467593e3fab08f4add3
vswapna3202 <vswapna3202@gmail.com>
2023-10-19 19:10:40 +0100

    Feat: Code for useEffect

--- src/components/hooks/use_effect/use_effect.tsx
+++ src/components/hooks/use_effect/use_effect.tsx
@@ -1,13 +1,42 @@
-// make an api call using useEffect
+import {useEffect, useState} from 'react';
+
+interface Todo {
+    userId: string,
+    id: number,
+    title: string,
+    completed: boolean
+}
 
 export const APICall = () => {
+    const[json,setJson] = useState<Todo | null>(null);
 
     // Your code here!
-
+    useEffect(() => {
+        
+        // make an api call using useEffect
+        const jsonTypiCode = "https://jsonplaceholder.typicode.com/todos/1";
+            
+        const fetchTodo  = async(apiEndPoint: RequestInfo | URL) => {
+            
+            try{
+                const response = await fetch(apiEndPoint);
+                const json = await response.json();
+                setJson(json);
+                console.log(`${json}`);
+            }catch(error){
+                console.log(`${error}`);
+            }       
+        }
+        fetchTodo(jsonTypiCode);
+    },[]);        
+      
+        
     return (
         <>
             <h2>useEffect</h2>
-            <p>Your todo here!</p>
-        </>
-    )
-}
+                <p>{json ? JSON.stringify(json) : 'Loading...'}</p>
+            </>
+    );
+}   
+
+     
