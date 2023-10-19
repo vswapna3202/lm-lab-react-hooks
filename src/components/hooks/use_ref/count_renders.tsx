2d2bb4582c69fe15769e0fa603e96123e76ad4ed
vswapna3202 <vswapna3202@gmail.com>
2023-10-19 22:22:17 +0100

    Feat: Added useRef code

--- src/components/hooks/use_ref/count_renders.tsx
+++ src/components/hooks/use_ref/count_renders.tsx
@@ -1,21 +1,25 @@
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 
 export const CountRenders = () => {
 	const [value, setValue] = useState('');
-	const count = 1;
+	//const count = 1;
+	const count = useRef(0);
 
+	useEffect(() => {
+		count.current += 1;
+	});
 	return (
 		<>
 			<h2>useRef</h2>
 
 			<input
 				value={value}
-				type='text'
+				type='text'				
 				onChange={(e) => setValue(e.target.value)}
 			/>
 
 			<p>{value}</p>
-			<p>I have rendered {count} times</p>
+			<p>I have rendered {count.current} times</p>
 		</>
 	);
 };
--- src/components/hooks/use_ref/focus.tsx
+++ src/components/hooks/use_ref/focus.tsx
@@ -10,7 +10,7 @@ export const Focus: React.FC = () => {
 
   return (
     <div>
-      <input type="text" />
+      <input ref={inputRef} type="text" />
       <button onClick={handleClick}>Focus Input</button>
     </div>
   );
