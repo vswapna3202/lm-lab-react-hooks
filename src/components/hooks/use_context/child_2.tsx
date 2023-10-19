0c23562ba5b571008edfc029542a9912b05cf4a9
vswapna3202 <vswapna3202@gmail.com>
2023-10-19 15:22:39 +0100

    Refactor: refactored code to use useContext

--- src/components/hooks/use_context/child_2.tsx
+++ src/components/hooks/use_context/child_2.tsx
@@ -1,10 +1,7 @@
-export const Child2: React.FC<{ dark: boolean }> = ({ dark }) => {
-	const theme = {
-		backgroundColor: dark ? '#333' : '#CCC',
-		color: dark ? 'white' : '#333',
-		padding: '2rem',
-		margin: '2rem',
-	};
+import {useContext} from 'react';
+import ThemeContext from "./parent";
 
+export const Child2 = () => {
+	const theme = useContext(ThemeContext);
 	return <div style={theme}>Child 2</div>;
 };
--- src/components/hooks/use_context/parent.tsx
+++ src/components/hooks/use_context/parent.tsx
@@ -1,22 +1,36 @@
-import { useState } from 'react';
+import { createContext, useState } from 'react';
 import { Child1 } from './child_1';
 import { Child2 } from './child_2';
 
+
+const ThemeContext = createContext({
+                        backgroundColor: '',
+                        color: '',
+                        padding: '',
+                        margin: ''});
 export const Parent = () => {
-
-    const [darkTheme, setDarkTheme] = useState(true)
-
+    const [darkTheme, setDarkTheme] = useState(true);
     const toggleTheme = () => {
         setDarkTheme(prevDarkTheme => !prevDarkTheme)
-    }
+    };
+
+    const theme = {
+		backgroundColor: darkTheme ? '#333' : '#CCC',
+		color: darkTheme ? 'white' : '#333',
+		padding: '2rem',
+		margin: '2rem',
+	};
 
     return (
         <div className='section'>
-            <h2>useContext</h2>
+            <h2>useContext</h2>            
             <button onClick={toggleTheme}>Toggle Theme</button>
-            <Child1 dark={darkTheme} />
-            <Child2 dark={darkTheme} />
+            <ThemeContext.Provider value={theme}>
+              <Child1 />
+              <Child2 />
+            </ThemeContext.Provider>
         </div>
 
     )
 }
+export default ThemeContext;
