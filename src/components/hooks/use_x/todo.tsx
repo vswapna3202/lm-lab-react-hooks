2b7ea96119fe408072969b4be247a2981c14c6ae
vswapna3202 <vswapna3202@gmail.com>
2023-10-20 08:02:33 +0100

    Feat: Added custom react hook

--- src/components/hooks/use_x/todo.tsx
+++ src/components/hooks/use_x/todo.tsx
@@ -1,38 +1,8 @@
-import { useEffect, useState } from 'react';
-import { isError } from '../../../helpers/is_error';
-
-/** This is the response that TypiCode gives for the /todos/ endpoint */
-interface TodoResponse {
-	userId: number;
-	id: number;
-	title: string;
-	completed: boolean;
-}
+import useFetch from './use_todo';
 
 export const Todo = () => {
-	const [data, setData] = useState<TodoResponse>();
-	const [isFetching, setIsFetching] = useState(true);
-
 	const url = 'https://jsonplaceholder.typicode.com/todos/1';
-
-	useEffect(() => {
-		const fetchData = async () => {
-			try {
-				const response = await fetch(url);
-				setIsFetching(false);
-				if (response.status === 200) {
-					const json = await response.json();
-					setData(json);
-				}
-			} catch (e: unknown) {
-				setIsFetching(false);
-
-				console.log(isError(e) ? e.message : 'Unknown error!');
-			}
-		};
-		fetchData();
-	}, [url]);
-
+	const {data, isFetching} = useFetch(url)
 	return (
 		<>
 			<h2>Custom Hook</h2>
