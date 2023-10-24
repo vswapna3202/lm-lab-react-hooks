5d65515b05474503efb097e9d1e26f9c5bb86e1b
vswapna3202 <vswapna3202@gmail.com>
2023-10-24 18:21:17 +0100

    Refactor: refactored code

--- src/components/hooks/custom_swapi_api/custom_swapi_hook.tsx
+++ src/components/hooks/custom_swapi_api/custom_swapi_hook.tsx
@@ -1,53 +1,53 @@
-import {useEffect, useState} from 'react';
+import { useState, useEffect } from "react";
 
-interface swapiPeopleData {
+interface swapiPeopleData{
     name: string;
+    birthYear: string;
+    gender: string;
     height: string;
     mass: string;
-    gender: string;
-    birthYear: string;
     starships: Array<string>;
 }
 
 const SwapiAPICall = () => {
-    const[swapiResultsData, setSwapiResultsData] = useState<Array<swapiPeopleData>>([]);
+    const[swapiResults,setSwapiResults] = useState<Array<swapiPeopleData>>([]);
     useEffect(() => {
         const url = "https://swapi.dev/api/people/";
         const getSwapiJSON = async(apiEndPoint: RequestInfo | URL) => {
             try{
                 const apiResponse = await fetch(apiEndPoint);
-                const jsonData = await apiResponse.json();
-                setSwapiResultsData(jsonData.results);
+                const jsonResults = await apiResponse.json();
+                setSwapiResults(jsonResults.results);
             }catch(error){
-                console.error("Error in async function swapAPICall");
-            }    
+                console.error("Error in async function swapiAPICall");
+            }   
         }
         getSwapiJSON(url);
     },[]);
     return(
         <>
         <h2>Swapi API Call</h2>
-        {swapiResultsData && swapiResultsData.length > 0 ? (
-            swapiResultsData.map((person, index) => (
-                <div key={index} className="list__item">
-                  <p className="label-container">Name: {person.name}</p>
-                  <p className="label-container">Birth Year: {person.birthYear}</p>
-                  <p className="label-container">Height: {person.height}</p>
-                  <p  className="label-container">Mass: {person.mass}</p>
-                  <p className="label-container">Gender: {person.gender}</p>
-                  <p className="label-container">Star Ships:</p>
-                  {person.starships && person.starships.length > 0 && (
-                  <ul>
-                    {person.starships.map((starship, idx) => (
-                        <li key={idx} >
-                            <a href={starship}>{starship}</a>
-                        </li>
-                    ))}
-                  </ul>
-                  )}
-                </div>                
+        {swapiResults && swapiResults.length > 0 ? (
+            swapiResults.map((person, index) => (
+                <div key={index} className='list__item'>
+                    <p className='label-container'>Name: {person.name}</p>
+                    <p className='label-container'>Birth Year: {person.birthYear}</p>
+                    <p className='label-container'>Gender: {person.gender}</p>
+                    <p className='label-container'>Height: {person.height}</p>
+                    <p className='label-container'>Mass: {person.mass}</p>
+                    {person.starships && person.starships.length > 0 && (
+                        <ul>
+                            {person.starships.map((starship,idx) => (
+                                <li key={idx} >
+                                    <a href={starship}>{starship}</a>
+                                </li>
+                            ))}
+                        </ul>
+                    )}
+                </div>
             ))
-        ) : <div className='list__name'>Loading...</div>}
+        ) : <div className='list__item'>Loading...</div>
+            }
         </>
     );
 }
